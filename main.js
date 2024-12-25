be7f8401eaa796d959b53d479dffbc7da7d3d68d
Papa1999 <thioubepapamacoumba@gmail.com>
2024-12-25 16:44:29 +0100

    Just some comments added

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Basic 3 Components
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -8,11 +10,19 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// Creating a 3D object with material
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
+
+// Adding it to the scene
 scene.add(cube);
+
+// Positionning the camera
 camera.position.z = 5;
+
+// Rendering 
 function animate() {
   renderer.render(scene, camera);
 }
