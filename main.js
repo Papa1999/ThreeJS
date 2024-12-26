365a587a8dd8252a4bed1769d8776136cca29e14
Papa1999 <thioubepapamacoumba@gmail.com>
2024-12-26 17:04:11 +0100

    Stuck in the orbitControl point

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-// Basic 3 Components
-
+// Initialize the SCENE
 const scene = new THREE.Scene();
+
+// Creating and adding object to the scene
+const cube = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+
+const cubeMesh = new THREE.Mesh(cube, material);
+scene.add(cubeMesh);
+
+// Initialize and position the CAMERA
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  100
 );
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Creating a 3D object with material
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-
-// Adding it to the scene
-scene.add(cube);
-
-// Positionning the camera
 camera.position.z = 5;
 
-// Rendering 
-function animate() {
+// Initialize the RENDERER
+const canvas = document.querySelector(".threejs");
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+// instatiate the controls
+const controls = new THREE.OrbitControls(camera, canvas);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const renderLoop = () => {
+  controls.update();
   renderer.render(scene, camera);
-}
-renderer.setAnimationLoop(animate);
+  requestAnimationFrame(renderLoop);
+};
+
+renderLoop();
