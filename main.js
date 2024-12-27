2f6c542e2b76b9234b5d01b550a50614c5cecda6
Papa1999 <thioubepapamacoumba@gmail.com>
2024-12-27 16:55:02 +0100

    OrbitControls's problem solved just by using the official documentation of three js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-console.log(THREE);
-// Initialize the SCENE
+/* 
+    Initialize the SCENE
+*/
 const scene = new THREE.Scene();
 
 // Creating and adding object to the scene
@@ -11,27 +13,45 @@ const material = new THREE.MeshBasicMaterial({ color: "red" });
 const cubeMesh = new THREE.Mesh(cube, material);
 scene.add(cubeMesh);
 
-// Initialize and position the CAMERA
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
+/* 
+    Initialize and position the CAMERA
+*/
+const aspectRatio = window.innerWidth / window.innerHeight;
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   aspectRatio,
+//   0.1,
+//   100
+// );
+
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio,
+  1 * aspectRatio,
+  1 ,
+  -1,
   0.1,
-  100
+  200
 );
 camera.position.z = 5;
 
-// Initialize the RENDERER
+/* 
+    Initialize the RENDERER
+*/
 const canvas = document.querySelector(".threejs");
 const renderer = new THREE.WebGLRenderer({ canvas });
-
-// instatiate the controls
-const controls = new THREE.OrbitControls(camera, canvas);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+/* 
+    instatiate the ORBITCONTROLS
+*/
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+
+// Displaying in the browser
 const renderLoop = () => {
   controls.update();
   renderer.render(scene, camera);
   requestAnimationFrame(renderLoop);
 };
-
 renderLoop();
